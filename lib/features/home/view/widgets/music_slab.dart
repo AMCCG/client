b5844038ee60ac9ff9e4d9294e3d4532597637ca
Apisit <amornchanchaigul@gmaul.com>
2024-12-09 08:37:50 +0700

    play/pause music

--- lib/features/home/view/widgets/music_slab.dart
+++ lib/features/home/view/widgets/music_slab.dart
@@ -11,6 +11,7 @@ class MusicSlab extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentSong = ref.watch(currentSongNotifierProvider);
+    final songNotifier = ref.read(currentSongNotifierProvider.notifier);
     if (currentSong == null) {
       return const SizedBox();
     }
@@ -66,12 +67,16 @@ class MusicSlab extends ConsumerWidget {
                 children: [
                   IconButton(
                     onPressed: () {},
-                    icon: const Icon(CupertinoIcons.heart),
+                    icon: const Icon(
+                      CupertinoIcons.heart,
+                    ),
                   ),
                   IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      CupertinoIcons.play_fill,
+                    onPressed: songNotifier.playPause,
+                    icon: Icon(
+                      songNotifier.isPlaying
+                          ? CupertinoIcons.pause_fill
+                          : CupertinoIcons.play_fill,
                       color: Pallete.whiteColor,
                     ),
                   ),
@@ -80,18 +85,31 @@ class MusicSlab extends ConsumerWidget {
             ],
           ),
         ),
-        Positioned(
-          bottom: 0,
-          left: 8,
-          child: Container(
-            height: 2,
-            width: 20,
-            decoration: BoxDecoration(
-              color: Pallete.whiteColor,
-              borderRadius: BorderRadius.circular(7),
-            ),
-          ),
-        ),
+        StreamBuilder(
+            stream: songNotifier.audioPlayer?.positionStream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SizedBox();
+              }
+              final position = snapshot.data;
+              final duration = songNotifier.audioPlayer!.duration;
+              double sliderValue = 0.0;
+              if (position != null && duration != null) {
+                sliderValue = position.inMicroseconds / duration.inMicroseconds;
+              }
+              return Positioned(
+                bottom: 0,
+                left: 8,
+                child: Container(
+                  height: 2,
+                  width: sliderValue * (MediaQuery.of(context).size.width - 32),
+                  decoration: BoxDecoration(
+                    color: Pallete.whiteColor,
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                ),
+              );
+            }),
         Positioned(
           bottom: 0,
           left: 8,
